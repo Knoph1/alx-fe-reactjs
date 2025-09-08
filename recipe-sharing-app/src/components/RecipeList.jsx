f08bd5ef528ab984fe92002fa730ce8a17c9d449
knoph1 <knopholuoch@yahoo.com>
2025-09-08 20:47:24 +0300

    Task 1: Rectify/Fixed RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,28 +1,7 @@
+// src/components/RecipeList.jsx
 import React from 'react';
 import RecipeCard from './RecipeCard';
 import { useRecipeStore } from '../store/recipeStore';
-import { Link } from 'react-router-dom';
-
-const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
-
-  return (
-    <div>
-      <h2>Recipe List</h2>
-      {recipes.length === 0 ? (
-        <p>No recipes yet. Add one above!</p>
-      ) : (
-        recipes.map((recipe) => (
-          <div key={recipe.id} style={{ marginBottom: '1rem' }}>
-            <h3>
-              <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
-            </h3>
-          </div>
-        ))
-      )}
-    </div>
-  );
-};
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
