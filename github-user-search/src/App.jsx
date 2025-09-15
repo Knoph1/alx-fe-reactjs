31eb6cd52720a3b5d94ab06f71b5db5d18cc7ccf
knoph1 <knopholuoch@yahoo.com>
2025-09-15 14:07:50 +0300

    Cleaner App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 import Search from "./components/Search";
 import SearchResults from "./components/SearchResults";
 import { searchUsers } from "./services/githubService";
 
 function App() {
-  const [count, setCount] = useState(0)
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
@@ -26,40 +22,19 @@ function App() {
   };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-        <h1 className="text-3xl font-bold text-blue-600 mb-6">
-          GitHub User Search
-        </h1>
-        <Search onSearch={handleSearch} />
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
+      <h1 className="text-3xl font-bold text-blue-600 mb-6">
+        GitHub User Search
+      </h1>
 
-        {loading && <p className="text-center mt-4">Loading...</p>}
-        {error && <p className="text-center text-red-500 mt-4">{error}</p>}
+      <Search onSearch={handleSearch} />
 
-        <SearchResults users={users} />
-      </div>
-    </>
-  )
+      {loading && <p className="text-center mt-4">Loading...</p>}
+      {error && <p className="text-center text-red-500 mt-4">{error}</p>}
+
+      <SearchResults users={users} />
+    </div>
+  );
 }
 
-export default App
+export default App;
