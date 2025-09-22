f26a92f1061f20d9f9d5240c1c257de73f9d70b0
knoph1 <knopholuoch@yahoo.com>
2025-09-22 23:00:48 +0300

    Task 2: Finalization

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,9 @@ function UserProfile() {
         p-2 sm:p-4 md:p-8 
         max-w-xs sm:max-w-sm md:max-w-sm 
         mx-auto my-10 sm:my-16 md:my-20 
-        rounded-lg shadow-lg
+        rounded-lg shadow-lg 
+        hover:shadow-xl 
+        transition-shadow duration-300 ease-in-out
       "
     >
       <img
@@ -15,15 +17,19 @@ function UserProfile() {
         className="
           rounded-full 
           mx-auto 
-          w-20 h-20 sm:w-24 sm:h-24 md:w-36 md:h-36
+          w-20 h-20 sm:w-24 sm:h-24 md:w-36 md:h-36 
+          transition-transform duration-300 ease-in-out 
+          hover:scale-110
         "
       />
       <h1
         className="
           text-base sm:text-lg md:text-xl 
           text-blue-800 
-          my-2 sm:my-3 md:my-4
-          text-center
+          my-2 sm:my-3 md:my-4 
+          text-center 
+          transition-colors duration-300 ease-in-out 
+          hover:text-blue-500
         "
       >
         John Doe
